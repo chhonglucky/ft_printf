86f4d815d83c562d055186ee58ada7e2ed4b36e1
Chanhyung Hong <chanhhon@c4r2s5.42seoul.kr>
2024-01-10 14:59:26 +0900

    dd

---
+++ Libft/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanhhon <chanhhon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 15:08:12 by chanhhon          #+#    #+#             */
+/*   Updated: 2023/12/09 17:53:31 by chanhhon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_itoa(int n);
+
+#endif
